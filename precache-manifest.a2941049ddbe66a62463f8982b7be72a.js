a7704a2fe0e0d58f69cb58fdaa02cdd7215e2bce
Solomon Basha <sol34basha1@outlook.com>
2021-04-16 21:09:19 +1000

    Updates

---
+++ precache-manifest.a2941049ddbe66a62463f8982b7be72a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb5ed37829c7f7b865d94219743e5391",
+    "url": "/solomonjbasha/Expense-Tracker.git/index.html"
+  },
+  {
+    "revision": "23d535204277db967d85",
+    "url": "/solomonjbasha/Expense-Tracker.git/static/css/main.708a4d9b.chunk.css"
+  },
+  {
+    "revision": "bd6bd55d0f17c3088198",
+    "url": "/solomonjbasha/Expense-Tracker.git/static/js/2.b190f742.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/solomonjbasha/Expense-Tracker.git/static/js/2.b190f742.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23d535204277db967d85",
+    "url": "/solomonjbasha/Expense-Tracker.git/static/js/main.e1a886a3.chunk.js"
+  },
+  {
+    "revision": "bcbee64e1f8baedabef5",
+    "url": "/solomonjbasha/Expense-Tracker.git/static/js/runtime-main.cf8fbc37.js"
+  }
+]);
